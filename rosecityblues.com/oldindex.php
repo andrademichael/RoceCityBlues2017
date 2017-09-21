85ef612ae19f5d972db2a3464744786a2a7929f0
michael <andrade.michael.e@gmail.com>
2017-09-20 17:37:33 -0700

    index.html is now king

--- rosecityblues.com/oldindex.php
+++
@@ -1,6 +0,0 @@
-<head>
-<title>Rose City Blues</title>
-
-<?php require_once('m_hat.php'); ?>
-          <img src="images/i_03f.gif" alt="Be There. November 6-8, 2015." width="652" height="652" class="nonothing"/>
-<?php require_once('m_shoes.php'); ?>
